d1d3ab2be4e69d9748ae3d68d81aa50c76d0b4a5
JackieMina <53856112@qq.com>
2024-03-07 17:02:43 +0800

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;//模型工厂相关功能的引用
+use Illuminate\Foundation\Auth\User as Authenticatable;//授权相关功能的引用
+use Illuminate\Notifications\Notifiable;//消息通知相关功能引用
+use Laravel\Sanctum\HasApiTokens; //API 令牌修改功能
 
 class User extends Authenticatable
 {
@@ -16,6 +16,7 @@ class User extends Authenticatable
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
+     * $fillable属性：在过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新
      */
     protected $fillable = [
         'name',
@@ -27,6 +28,7 @@ class User extends Authenticatable
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
+     * $hidden属性：对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏
      */
     protected $hidden = [
         'password',
@@ -37,8 +39,17 @@ class User extends Authenticatable
      * The attributes that should be cast.
      *
      * @var array<string, string>
+     * $casts属性：是用来指定数据库字段使用的数据类型
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * 用来生成用户的头像
+     */
+    public function gravatar($size='100'){
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "https://cdn.v2ex.com/gravatar/$hash?s=$size";
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 在 Laravel 5.4 及以下版本，使用 Schema::defaultStringLength 方法
+    Schema::defaultStringLength(191);
+
+    // 或者在 Laravel 5.5 及更高版本，使用 Schema::defaultStringLength 方法的别名
+    \Illuminate\Database\Schema\Builder::defaultStringLength(191);
     }
 }
