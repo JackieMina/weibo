3c963d73b67df577d3c9b60aed047b101326c498
JackieMina <53856112@qq.com>
2024-03-11 14:25:26 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,18 +8,15 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
-    public function create()
-    {
+    public function create(){
         return view('users.create');
     }
 
-    public function show(User $user)
-    {
+    public function show(User $user){
         return view('users.show', compact('user'));
     }
 
-    public function store(Request $request)
-    {
+    public function store(Request $request){
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
@@ -35,5 +32,26 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|min:6'
+        ]);
+
+        $data=[];
+        $data['name']=$request->name;
+        if($request->password){
+            $data['password']=bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success','update success! ');
+
+        return redirect()->route('users.show',$user);
     }
 }
