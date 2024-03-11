0fc7d8ff899f50d499ce009d744796c79e182d74
JackieMina <53856112@qq.com>
2024-03-11 15:42:44 +0800

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends Controller
 {
     public function __construct(){
         $this->middleware('auth', [            
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store','index']
         ]);
     
         $this->middleware('guest',[
@@ -65,5 +65,9 @@ class UsersController extends Controller
         session()->flash('success','update success! ');
 
         return redirect()->route('users.show',$user);
+    }
+    public function index(){
+        $users=User::all();
+        return view('users.index',compact('users'));
     }
 }
