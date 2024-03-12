dae42761d88a9e09d95cb136e0fb64835a0b6df8
JackieMina <53856112@qq.com>
2024-03-12 11:27:26 +0800

    首页微博列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,6 +7,9 @@
         <section class="status_form">
           @include('shared._status_form')
         </section>
+        <h4>微博列表</h4>
+        <hr>
+        @include('shared._feed')
       </div>
       <aside class="col-md-4">
         <section class="user_info">
@@ -18,7 +21,7 @@
     <div class="bg-light p-3 p-sm-5 rounded">
       <h1>Hello Laravel</h1>
       <p class="lead">
-        你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel</a> 的示例项目主页。
+        你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
       </p>
       <p>
         一切，将从这里开始。
